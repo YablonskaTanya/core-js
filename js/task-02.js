30ddeadfdbcbca742c9d9abc0d933c2dbe90f359
Yablonskaya_Tanya <t.n.yablonskaya@gmail.com>
2023-11-17 19:42:18 +0200

    modified:   js/task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const item = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+
+  li.textContent = ingredient;
+  li.classList.add("item");
+
+  return li;
+});
+
+list.append(...item);
+console.log("ingredient :>> ", item);
