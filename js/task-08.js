392ec39c5b0cf394828770b926782bfa8eb61e77
Yablonskaya_Tanya <t.n.yablonskaya@gmail.com>
2023-11-18 18:10:54 +0200

    modified:   js/task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
+console.log("form :>> ", form);
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const email = e.currentTarget.elements.email.value;
+  const password = e.currentTarget.elements.password.value;
+  if (!email || !password) {
+    alert("всі поля повинні бути заповнені");
+  } else {
+    console.log(`Email: ${email},\nPassword: ${password}`);
+  }
+  form.reset();
+}
