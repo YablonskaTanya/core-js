2f38d9d8a233ee25cb10399cca3b590eae11952a
Yablonskaya_Tanya <t.n.yablonskaya@gmail.com>
2023-11-18 18:30:42 +0200

    modified:   js/task-08.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const body = document.querySelector("body");
+
+buttonEl.addEventListener("click", onHandleClick);
+
+function onHandleClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
