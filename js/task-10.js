d9f9b773bf1d91c165e40161e5b1f4bb9e3f67ba
Yablonskaya_Tanya <t.n.yablonskaya@gmail.com>
2023-12-05 17:55:59 +0200

    add fix

--- js/task-10.js
+++ js/task-10.js
@@ -11,6 +11,20 @@ destroyBtn.addEventListener("click", destroyBoxes);
 // function createBoxes(amount) {}
 // function destroyBoxes() {}
 
+// Створи функцію createBoxes(amount),
+//яка приймає один параметр - число.
+//Функція створює стільки <div>, скільки
+//вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим
+//і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX.
+// Використовуй готову функцію getRandomHexColor для отримання кольору.
+
+//Створи функцію destroyBoxes(), яка очищає вміст div#boxes,
+//у такий спосіб видаляючи всі створені елементи.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
