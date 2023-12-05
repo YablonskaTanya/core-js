e4281e063a5d7667f0fc0bf8c839d978c15a133a
Yablonskaya_Tanya <t.n.yablonskaya@gmail.com>
2023-12-05 13:11:39 +0200

    add change task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,33 +28,33 @@ function onModalShow(e) {
   const sourceData = e.target.getAttribute("data-source");
 
   const instance = basicLightbox.create(
-    `<img src="${sourceData}" width="800" height="600">`
-    // {
-    //   onShow: () => {
-    //     gallery.addEventListener("keydown", onCloseModalByEsc);
-    //   },
-    //   onClose: () => {
-    //     gallery.removeEventListener("keydown", onCloseModalByEsc);
-    //   },
-    // }
+    `<img src="${sourceData}" width="800" height="600">`,
+    {
+      onShow: () => {
+        gallery.addEventListener("keydown", onCloseModalByEsc);
+      },
+      onClose: () => {
+        gallery.removeEventListener("keydown", onCloseModalByEsc);
+      },
+    }
   );
-
-  // function onCloseModalByEsc(e) {
-  //   console.log("e.code :>> ", e.code);
-  //   if (e.code === "Escape") {
-  //     instance.close();
-  //   }
-  // }
-
-  instance.show();
-
-  gallery.addEventListener("keydown", onCloseModalByEsc);
 
   function onCloseModalByEsc(e) {
     console.log("e.code :>> ", e.code);
     if (e.code === "Escape") {
       instance.close();
-      gallery.removeEventListener("keydown", onCloseModalByEsc);
     }
   }
+
+  instance.show();
+
+  // gallery.addEventListener("keydown", onCloseModalByEsc);
+
+  // function onCloseModalByEsc(e) {
+  //   console.log("e.code :>> ", e.code);
+  //   if (e.code === "Escape") {
+  //     instance.close();
+  //     gallery.removeEventListener("keydown", onCloseModalByEsc);
+  //   }
+  // }
 }
